7ac710d7b828511988008ae9f7671b3117c517aa
Munaf Badarpura <munafbadarpura11@gmail.com>
2025-06-22 11:51:31 +0530

    updated stripe config

--- src/main/java/com/munaf/bookingEase/configs/StripConfig.java
+++ src/main/java/com/munaf/bookingEase/configs/StripConfig.java
@@ -2,14 +2,24 @@ package com.munaf.bookingEase.configs;
 
 
 import com.stripe.Stripe;
+import jakarta.annotation.PostConstruct;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class StripConfig {
 
-    public StripConfig(@Value("${stripe.secret.key}") String stripSecretKey) {
-        Stripe.apiKey = stripSecretKey;
+    @Value("${stripe.secret.key}")
+    private String stripSecretKey;
+
+    @PostConstruct
+    public void init() {
+        Stripe.apiKey = stripSecretKey; // setting up key in stripe
     }
+
+
+//    public StripConfig(@Value("${stripe.secret.key}") String stripSecretKey) {
+//        Stripe.apiKey = stripSecretKey;
+//    }
 
 }
